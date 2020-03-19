d59408652e9f4331e1d32a577b3407c8fab084b1
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-19 22:02:37 +0100

    Add project files.

---
+++ division/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int result;
+
+            Console.Write("Enter your first number: ");
+            number1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter your Second number: ");
+            number2 = Int32.Parse(Console.ReadLine());
+
+            result = number1 / number2;
+            Console.WriteLine("Your result is "+ result);
+        }
+    }
+}
